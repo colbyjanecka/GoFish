af6f7949e551b10b1b1d738a9831d89ec91af557
Colby Janecka <colbyjanecka@aol.com>
2019-04-11 18:16:23 -0500

    format changes

--- player.h
+++ player.h
@@ -39,35 +39,35 @@ public:
 	// this function will check a players hand for a pair.
 	// If a pair is found, it returns true and populates the two variables with the cards tha make the pair.
 
-	bool checkHandForBook(Card &c1, Card &c2);
+	bool checkHandForBook(Card &c1, Card &c2);	// done
 
-									// OPTIONAL
-									// comment out if you decide to not use it
-									// Does the player have a card with the same rank as c in her hand?
+												// OPTIONAL
+												// comment out if you decide to not use it
+												// Does the player have a card with the same rank as c in her hand?
 	bool rankInHand(Card c) const;
 
-									// uses some strategy to choose one card from the player's
-									// hand so they can say "Do you have a 4?"
-	Card chooseCardFromHand() const;// done
+												// uses some strategy to choose one card from the player's
+												// hand so they can say "Do you have a 4?"
+	Card chooseCardFromHand() const;			// done
 
-									// Does the player have the card c in her hand?
-	bool cardInHand(Card c) const;	// done
+												// Does the player have the card c in her hand?
+	bool cardInHand(Card c) const;				// done
 
-									// Remove the card c from the hand and return it to the caller
-	Card removeCardFromHand(Card c);// done
+												// Remove the card c from the hand and return it to the caller
+	Card removeCardFromHand(Card c);			// done
 
-	string showHand() const;		// done
-	string showBooks() const;		// done
+	string showHand() const;					// done
+	string showBooks() const;					// done
 
-	int getHandSize() const;		// done
-	int getBookSize() const;		// DONE
+	int getHandSize() const;					// done
+	int getBookSize() const;					// DONE
 
 	// OPTIONAL
 	// comment out if you decide to not use it
 	// this function will check a players hand for a pair.
 	// If a pair is found, it returns true and populates the two variables with the cards tha make the pair.
 
-	bool checkHandForPair(Card &c1, Card &c2);
+	bool checkHandForPair(Card &c1, Card &c2);	// using checkHandForBook
 
 
 	// OPTIONAL

cd5817a9dc815d16e320698d6945498078ea72c7
Colby Janecka <colbyjanecka@aol.com>
2019-04-11 14:37:49 -0500

    Added 'addCard' and 'showHand' methods to player class.

--- player.cpp
+++ player.cpp
@@ -2,3 +2,23 @@
 #include <string>
 using namespace std;
 
+Player::Player()
+{
+    myName = " ";
+}
+
+void Player::addCard(Card c)
+{
+    myHand.push_back(c);
+}
+
+string Player::showHand() const
+{
+    string contents;
+
+    vector<Card>::const_iterator iter;
+    for (iter = myHand.begin(); iter != myHand.end(); iter++) {
+        contents += (*iter).toString() + " ";
+    }
+    return (contents);
+}

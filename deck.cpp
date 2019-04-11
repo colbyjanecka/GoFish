025ab14886e23cb1119fa7892274817182d246dc
coreykarnei <31517198+coreykarnei@users.noreply.github.com>
2019-04-10 22:43:18 -0500

    Add files via upload

--- deck.cpp
+++ deck.cpp
@@ -1,3 +1,14 @@
 #include "deck.h"
 #include <string>
 using namespace std;
+
+
+Deck::Deck() {
+    myIndex = 0;
+    for(Suit = Card::spades; Suit <= Card::clubs; Suit++ ){
+        for(int rank = 1; rank <= 14 ; rank++){
+            myCards[myIndex] = Card(rank, Suit);
+            myIndex++;
+        }
+    }
+}

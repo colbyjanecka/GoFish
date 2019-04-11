1cdfb5824cc9faf6d9b9ec95298dea2bf571b984
Colby Janecka <colbyjanecka@aol.com>
2019-04-11 18:17:26 -0500

    removed reduntant methods

--- player.h
+++ player.h
@@ -41,11 +41,6 @@ public:
 
 	bool checkHandForBook(Card &c1, Card &c2);	// done
 
-												// OPTIONAL
-												// comment out if you decide to not use it
-												// Does the player have a card with the same rank as c in her hand?
-	bool rankInHand(Card c) const;
-
 												// uses some strategy to choose one card from the player's
 												// hand so they can say "Do you have a 4?"
 	Card chooseCardFromHand() const;			// done
@@ -64,18 +59,10 @@ public:
 
 	// OPTIONAL
 	// comment out if you decide to not use it
-	// this function will check a players hand for a pair.
-	// If a pair is found, it returns true and populates the two variables with the cards tha make the pair.
-
-	bool checkHandForPair(Card &c1, Card &c2);	// using checkHandForBook
-
-
-	// OPTIONAL
-	// comment out if you decide to not use it
 	// Does the player have a card with the same rank as c in her hand?
 	// e.g. will return true if the player has a 7d and the parameter is 7c
 
-	bool sameRankInHand(Card c) const;
+	bool sameRankInHand(Card c) const;	// done
 
 
 private:

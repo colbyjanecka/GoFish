6592df817cd5943d5148efbae7751e00df0272aa
Colby Janecka <colbyjanecka@aol.com>
2019-04-11 17:24:41 -0500

    fixed spacing

--- deck.cpp
+++ deck.cpp
@@ -1,7 +1,7 @@
 #include "deck.h"
 #include <string>
 #include <ctime>
-#include <iostream> // Provides cout and cin
+#include <iostream>	// Provides cout and cin
 #include <cstdlib>
 
 using namespace std;
@@ -9,53 +9,52 @@ using namespace std;
 
 Deck::Deck()
 {
-    myIndex = 0;
-    int tempIndex = 0;
+	myIndex = 0;
+	int tempIndex = 0;
 
-    for (int suit = 0; suit < 4; suit++) {
-        for (int rank = 1; rank < 14; rank++) {
-            myCards[tempIndex] = Card(rank, Card::Suit(suit));
-            // cout<<myIndex<<". "<<myCards[myIndex].toString()<<endl;
-            tempIndex++;
-        }
-    }
-    // for(int j=0; j < SIZE; j++){
-    //    cout << myCards[j].toString() << endl;
-    // }
+	for (int suit = 0; suit < 4; suit++) {
+		for (int rank = 1; rank < 14; rank++) {
+			myCards[tempIndex] = Card(rank, Card::Suit(suit));
+// cout<<myIndex<<". "<<myCards[myIndex].toString()<<endl;
+			tempIndex++;
+		}
+	}
+	// for(int j=0; j < SIZE; j++){
+	//    cout << myCards[j].toString() << endl;
+	// }
 }
 
 void Deck::shuffle()
 {
-    Card temp;
+	Card temp;
 
-    srand(time(NULL) + getpid());
-    for (int i = 0; i < 512; i++) {
-        int i1 = rand() % SIZE;
-        int i2 = rand() % SIZE;
-        temp        = myCards[i1];
-        myCards[i1] = myCards[i2];
-        myCards[i2] = temp;
-    }
-    // for(int j =0; j< SIZE; j++){
-    //    cout << myCards[j].toString() << endl;
-    // }
+	for (int i = 0; i < 512; i++) {
+		int i1 = rand() % SIZE;
+		int i2 = rand() % SIZE;
+		temp        = myCards[i1];
+		myCards[i1] = myCards[i2];
+		myCards[i2] = temp;
+	}
+	// for(int j =0; j< SIZE; j++){
+	//    cout << myCards[j].toString() << endl;
+	// }
 }
 
 Card Deck::dealCard()
 {
-    Card temp(0, Card::Suit(0));
+	Card temp(0, Card::Suit(0));
 
-    if (size() > 0) {
-        temp = myCards[myIndex];
-        myIndex++;
-    }
+	if (size() > 0) {
+		temp = myCards[myIndex];
+		myIndex++;
+	}
 
-    return temp;
+	return temp;
 }
 
 int Deck::size() const
 {
-    int remainingCards = SIZE - myIndex;
+	int remainingCards = SIZE - myIndex;
 
-    return (remainingCards);
+	return (remainingCards);
 }

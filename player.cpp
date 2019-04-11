b4d05a2543902485b411a6759b7586b71a6ee40d
Colby Janecka <colbyjanecka@aol.com>
2019-04-11 16:16:39 -0500

    added showHand and getHandSize methods

--- player.cpp
+++ player.cpp
@@ -4,21 +4,37 @@ using namespace std;
 
 Player::Player()
 {
-    myName = " ";
+	myName = " ";
 }
 
 void Player::addCard(Card c)
 {
-    myHand.push_back(c);
+	myHand.push_back(c);
 }
 
 string Player::showHand() const
 {
-    string contents;
+	string contents;
 
-    vector<Card>::const_iterator iter;
-    for (iter = myHand.begin(); iter != myHand.end(); iter++) {
-        contents += (*iter).toString() + " ";
-    }
-    return (contents);
+	vector<Card>::const_iterator iter;
+	for (iter = myHand.begin(); iter != myHand.end(); iter++) {
+		contents += iter->toString() + " ";
+	}
+	return (contents);
+}
+
+Card Player::chooseCardFromHand() const
+{
+	Card c;
+}
+
+int Player::getHandSize() const
+{
+	int count = 0;
+
+	vector<Card>::const_iterator iter;
+	for (iter = myHand.begin(); iter != myHand.end(); iter++) {
+		count++;
+	}
+	return (count);
 }
